670535a361db84cc2fce96cea1505a2c74c738ad
zcnzdmr <ozcanozdemir001@gmail.com>
2024-07-28 20:53:17 +0300

    SignInVC textfields is completed by using CustomTextField class

--- SnapChat/SignInVC.swift
+++ SnapChat/SignInVC.swift
@@ -9,8 +9,10 @@ import UIKit
 
 class SignInVC: UIViewController {
     
-    
-    var headerView = AuthHeaderView(title: "Sign In", subtitle: "Sign in to your account")
+    // MARK: UI Components
+    let headerView = AuthHeaderView(title: "Sign In", subtitle: "Sign in to your account")
+    let usernameTF = CustomTextField(textFieldType: .username)
+    let password = CustomTextField(textFieldType: .password)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,15 +26,30 @@ class SignInVC: UIViewController {
     private func setUpUI() {
         
         self.view.addSubview(headerView)
+        self.view.addSubview(usernameTF)
+        self.view.addSubview(password)
+        
         headerView.translatesAutoresizingMaskIntoConstraints = false
+        usernameTF.translatesAutoresizingMaskIntoConstraints = false
+        password.translatesAutoresizingMaskIntoConstraints = false
         
         
         NSLayoutConstraint.activate([
             headerView.topAnchor.constraint(equalTo: self.view.topAnchor),
             headerView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
             headerView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
-
-            headerView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.3)
+            headerView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.3),
+            
+            usernameTF.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 300),
+            usernameTF.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            usernameTF.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.85),
+            usernameTF.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.05),
+            
+            password.topAnchor.constraint(equalTo: usernameTF.bottomAnchor, constant: 10),
+            password.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            password.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.85),
+            password.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.05)
+            
         ])
     }
     
--- SnapChat/Views/CustomTextField.swift
+++ SnapChat/Views/CustomTextField.swift
@@ -8,13 +8,79 @@
 import UIKit
 
 class CustomTextField: UITextField {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    enum TextFieldType {
+        case username
+        case email
+        case password
     }
-    */
-
+    
+    private let authFieldType : TextFieldType
+    
+//    private let userNameTF : UITextField  = {
+//        
+//        let textField = UITextField()
+//        textField.placeholder = "  username or email"
+//        textField.keyboardType = .emailAddress
+//        textField.textAlignment = .left
+//        textField.autocapitalizationType = .none
+//        textField.autocorrectionType = .no
+//        textField.textContentType = .emailAddress
+//        textField.layer.cornerRadius = 10
+//        textField.layer.borderColor = UIColor(.gray.opacity(0.8)).cgColor
+//        textField.layer.borderWidth = 1
+//        textField.font = .systemFont(ofSize: 15, weight: .medium)
+//        return textField
+//    }()
+    
+//    private let passwordTF : UITextField  = {
+//        
+//        let textField = UITextField()
+//        textField.placeholder = "  password"
+//        textField.keyboardType = .default
+//        textField.textAlignment = .left
+//        textField.autocapitalizationType = .none
+//        textField.autocorrectionType = .no
+//        textField.isSecureTextEntry = true
+//        textField.layer.cornerRadius = 10
+//        textField.layer.borderColor = UIColor(.gray.opacity(0.8)).cgColor
+//        textField.textContentType = .oneTimeCode
+//        textField.layer.borderWidth = 1
+//        textField.font = .systemFont(ofSize: 15, weight: .medium)
+//        return textField
+//    }()
+    
+    
+    init(textFieldType: TextFieldType) {
+        self.authFieldType = textFieldType
+        super.init(frame: .zero)
+        
+        self.backgroundColor = .secondarySystemBackground
+        self.textAlignment = .left
+        self.layer.borderWidth = 1
+        self.layer.cornerRadius = 10
+        self.autocapitalizationType = .none
+        self.autocorrectionType = .no
+        self.returnKeyType = .done
+        
+        if authFieldType == .username {
+            self.placeholder = "  username or email"
+            self.keyboardType = .emailAddress
+            self.textContentType = .emailAddress
+            
+        }
+        
+        
+        if authFieldType == .password {
+            self.placeholder = "  password"
+            self.isSecureTextEntry = true
+            self.textContentType = .oneTimeCode
+        }
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
