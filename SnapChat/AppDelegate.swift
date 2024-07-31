2346d052dde2d08741916d82f146982a0bbc0164
Özcan <ozcanozdemir001@gmail.com>
2024-07-31 16:45:34 +0300

    Firebase integrated with Cocoapods and app works properly

--- SnapChat/AppDelegate.swift
+++ SnapChat/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
+        FirebaseApp.configure()
         self.window = UIWindow(frame: UIScreen.main.bounds)
         window?.backgroundColor = .systemBackground
         
