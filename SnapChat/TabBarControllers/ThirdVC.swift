a53b17dfce6a3d8bacd9159b8523e73695d6c457
zcnzdmr <ozcanozdemir001@gmail.com>
2024-08-11 09:36:25 +0300

    ThirdVC designed and UIImagePickerController is added

--- SnapChat/TabBarControllers/ThirdVC.swift
+++ SnapChat/TabBarControllers/ThirdVC.swift
@@ -8,22 +8,78 @@
 import UIKit
 
 class ThirdVC: UIViewController {
+    
+    
+// MARK: UI Components
+    
+    var myImage : UIImageView = {
+        var iv = UIImageView()
+        iv.clipsToBounds = true
+        iv.layer.borderWidth = 1
+        iv.image = UIImage(named: "arkaplan")
+        iv.isUserInteractionEnabled = true
+
+        return iv
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpUIs()
 
         view.backgroundColor = .secondarySystemBackground
+        gestureRec()
+        self.navigationItem.title = "Upload Page"
+        self.navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.black,
+                                                                        .font: UIFont(name: "Papyrus", size: 20)!]
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func gestureRec() {
+        let gR = UITapGestureRecognizer(target: self, action: #selector(showImagePickerController))
+        myImage.addGestureRecognizer(gR)
     }
-    */
 
+    
+    private func setUpUIs() {
+        
+        self.view.addSubview(myImage)
+        
+        
+        myImage.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        NSLayoutConstraint.activate([
+            
+            myImage.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            myImage.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            myImage.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.6),
+            myImage.widthAnchor.constraint(equalTo: self.view.widthAnchor)
+        
+        ])
+        
+    }
+
+}
+
+// MARK: Picking Up photos from gallery
+extension ThirdVC : UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @objc func showImagePickerController() {
+        let imagePickerCont = UIImagePickerController()
+        imagePickerCont.delegate = self
+        imagePickerCont.allowsEditing = true
+        present(imagePickerCont, animated: true,completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let editedImage = info[.editedImage] as? UIImage {
+            myImage.image = editedImage
+        }else if let originalImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            myImage.image = originalImage
+        }else{
+            AlertManager.showGeneralAlert(on: self, title: "Something went wrong", message: "Try again")
+        }
+        dismiss(animated: true)
+    }
+    
 }
